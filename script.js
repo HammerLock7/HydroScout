40abc38ae37602be91fc384d01855f72a5013aa4
Lebakang Mathe <tsheposhaun7@gmail.com>
2024-09-01 13:59:26 +0200

    addee lines

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('signUpButton').onclick = function() {
     window.location.href = 'signIn.html';
 };
+
+document.getElementById('fgtPswd').onclick = function() {
+    window.location.href = 'ForgotPassword.html';
+};
